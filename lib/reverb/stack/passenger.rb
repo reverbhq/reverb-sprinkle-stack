cd9ef0b6d22050dbec8c2f7b7cfdc1260c23ab67
Weston Triemstra <weston@netsign.com>
2012-08-22 16:43:22 -0700

    reorganized to support vhosts

--- lib/reverb/stack/passenger.rb
+++ lib/reverb/stack/passenger.rb
@@ -15,7 +15,11 @@ package :nginx_passenger do
   description "Configure nginx installed through passenger"
   requires :passenger
 
-  push_text File.read(File.join(File.dirname(__FILE__), 'nginx', 'init.d')), "/etc/init.d/nginx", :sudo => true do
+  push_text File.read(File.join(File.dirname(__FILE__),'nginx','usr','local','nginx','conf','nginx.conf')), "/usr/local/nginx/conf/nginx.conf", :sudo => true do
+    post :install, "sudo mkdir -p /usr/local/nginx/conf/sites-enabled"
+  end
+
+  push_text File.read(File.join(File.dirname(__FILE__),'nginx','etc','init.d','nginx')), "/etc/init.d/nginx", :sudo => true do
     post :install, "sudo chmod +x /etc/init.d/nginx"
     post :install, "sudo /usr/sbin/update-rc.d -f nginx defaults"
     post :install, "sudo /etc/init.d/nginx start"
@@ -24,5 +28,7 @@ package :nginx_passenger do
   verify do
     has_executable "/usr/local/nginx/sbin/nginx"
     has_file "/etc/init.d/nginx"
+    has_file "/usr/local/nginx/conf/nginx.conf"
+    has_directory "/usr/local/nginx/conf/sites-enabled"
   end
 end
